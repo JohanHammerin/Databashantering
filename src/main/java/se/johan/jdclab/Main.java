8c3942893197bf3831ca85c6af04e9a000c0309e
JohanHammerin <Johan04hammerin@gmail.com>
2024-11-21 18:13:12 +0100

    18:13

--- src/main/java/se/johan/jdclab/Main.java
+++ src/main/java/se/johan/jdclab/Main.java
@@ -1,11 +1,15 @@
 package se.johan.jdclab;
 
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
 
         System.out.println(JDBCUtil.getDatabaseProductName(JDBCUtil.getConnection()));
 
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        System.out.println(conn);
     }
 }

4397174a636d0e957229d44ef2cef869fd2848f2
JohanHammerin <Johan04hammerin@gmail.com>
2024-11-26 15:46:05 +0100

    17:05

---
+++ src/main/java/se/johan/jdclab/lektion_5/Main.java
@@ -0,0 +1,12 @@
+package se.johan.jdclab.lektion_5;
+
+import java.sql.Date;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        PersonDAOImpl dao = new PersonDAOImpl();
+        //dao.insertPerson(new Person("Håkan", "Glazeman", "M", Date.valueOf("2000-01-01"), 2000));
+        dao.selectAll();
+    }
+}

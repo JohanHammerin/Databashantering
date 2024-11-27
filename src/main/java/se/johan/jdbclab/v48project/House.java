142fce4c06c0aedfbb6d11247ccffe833d6f0ebe
johanhammerin <johan04hammerin@gmail.com>
2024-11-27 13:00:23 +0100

    13:00

---
+++ src/main/java/se/johan/jdbclab/v48project/House.java
@@ -0,0 +1,61 @@
+package se.johan.jdbclab.v48project;
+
+public class House {
+    //Attributes
+    private int houseId;
+    private String address;
+    private String city;
+    private String postalCode;
+    private double price;
+    private int personId;
+
+
+    //Getters & Setters
+    public int getHouseId() {
+        return this.houseId;
+    }
+
+    public void setHouseId(int houseId) {
+        this.houseId = houseId;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return this.postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getPersonId() {
+        return this.personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+}
---
+++ src/main/java/se/johan/jdbclab/v48project/Person.java
@@ -0,0 +1,97 @@
+package se.johan.jdbclab.v48project;
+
+import java.sql.Date;
+import java.util.Objects;
+
+public class Person {
+    //Attributes
+    private int personId;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private java.sql.Date dob;
+    private double income;
+
+
+    //Constructors
+    Person(int personID, String firstName, String lastName, String gender, Date dob, double income) {
+        setPersonId(personId);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setGender(gender);
+        setDob(dob);
+        setIncome(income);
+    }
+
+    Person(String firstName, String lastName, String gender, Date dob, double income) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setGender(gender);
+        setDob(dob);
+        setIncome(income);
+    }
+
+
+    //Getters & Setters
+    public int getPersonId() {
+        return this.personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDob() {
+        return this.dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public double getIncome() {
+        return this.income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+
+    //Equals & hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return personId == person.personId && Double.compare(income, person.income) == 0 && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(gender, person.gender) && Objects.equals(dob, person.dob);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(personId, firstName, lastName, gender, dob, income);
+    }
+}

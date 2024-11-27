49e5a0c78db2cd4029300411d3107a65fa5fe942
johanhammerin <johan04hammerin@gmail.com>
2024-11-27 20:26:31 +0100

    20:28

--- src/main/java/se/johan/jdbclab/v48project/Main.java
+++ src/main/java/se/johan/jdbclab/v48project/Main.java
@@ -3,6 +3,8 @@ package se.johan.jdbclab.v48project;
 import java.sql.Date;
 import java.sql.SQLException;
 
+
+
 public class Main {
     public static void main(String[] args) throws SQLException {
         PersonDAO personDAO = new PersonDAOImpl();
